92e49650529e69b62740391569714e2f61a2823f
ValentinStoyanov <56118614+ValentinStoyanov@users.noreply.github.com>
2019-11-17 12:53:49 +0100

    It1 completed implementation v.0.1.0

--- ProjectISO2/src/main/java/infomine/ProjectISO2/App.java
+++ ProjectISO2/src/main/java/infomine/ProjectISO2/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "---Starting app---" );
+        System.out.println( "----Starting app----" );
         
     }
 }
