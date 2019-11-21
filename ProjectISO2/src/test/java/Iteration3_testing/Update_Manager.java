84db28f2032d00782b589ca536f9d452fe4211e6
davidupup96 <55482517+davidupup96@users.noreply.github.com>
2019-11-21 10:47:15 +0100

    Added the testing of iteration 3 and some problem solved on iteration 6 testing

---
+++ ProjectISO2/src/test/java/Iteration3_testing/Update_Manager.java
@@ -0,0 +1,14 @@
+package Iteration3_testing;
+
+import org.testng.annotations.Test;
+
+import Iteration3.Interface_Update;
+
+public class Update_Manager implements Interface_Update{
+
+		@Test
+		public void updateWarehouse() {
+			
+		}
+		
+}
