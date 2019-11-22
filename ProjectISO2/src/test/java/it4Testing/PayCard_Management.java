2dd498f714d65179453fb6f4bf1cafa6cf0cab79
Angel Custodio <angelglvz99@gmail.com>
2019-11-22 19:03:09 +0100

    testing of iteration 4

---
+++ ProjectISO2/src/test/java/it4Testing/PayCard_Management.java
@@ -0,0 +1,10 @@
+package it4Testing;
+
+import org.testng.annotations.Test;
+
+import iteration4.Interface_PayCard;
+
+public class PayCard_Management implements Interface_PayCard{
+	@Test
+	public void payCreditCard() {}
+}
