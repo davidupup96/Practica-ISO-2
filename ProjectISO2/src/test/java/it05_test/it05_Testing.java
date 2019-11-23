0111294c0c40f6b348db91cb2535064fdac05919
PepeMola <48015034+PepeMola@users.noreply.github.com>
2019-11-23 19:04:55 +0100

    23/11/2019 Jose Antonio Arias Ramos
    -Added package it05_test
    -Added class it05_Testing

---
+++ ProjectISO2/src/test/java/it05_test/it05_Testing.java
@@ -0,0 +1,13 @@
+package it05_test;
+
+import org.testng.annotations.Test;
+
+import it05.interface_cancelReserve;
+
+public class it05_Testing {
+	@Test
+	public void cancelReserve() {
+		
+	}
+
+}
